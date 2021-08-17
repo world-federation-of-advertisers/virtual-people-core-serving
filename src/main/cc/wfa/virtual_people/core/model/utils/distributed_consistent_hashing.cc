cfc6b4fa8bb9e5aec9c46d3115e5320744f1e693
Kangxing Fu <kangxing@google.com>
2021-08-17 13:34:55 -0700

    Reduce the allowed error when doing nomailization check.

--- src/main/cc/wfa/virtual_people/core/model/utils/distributed_consistent_hashing.cc
+++ src/main/cc/wfa/virtual_people/core/model/utils/distributed_consistent_hashing.cc
@@ -30,7 +30,7 @@
 
 namespace wfa_virtual_people {
 
-constexpr double kNormalizeError = 0.01;
+constexpr double kNormalizeError = 0.0000001;
 
 absl::StatusOr<std::unique_ptr<DistributedConsistentHashing>>
 DistributedConsistentHashing::Build(
